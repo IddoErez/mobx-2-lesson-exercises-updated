06055ee238745fadeb13e3e55448b8922f33d1fb
Ido Erez <idodoerez@gmail.com>
2021-01-05 12:41:45 +0200

    finished mobx-2 exercises

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react'
 import ResInput from './ResInput';
+import Reservation from './Reservation';
 
-class Restaurant extends Component{
-    render () {
+class Restaurant extends Component {
+    addRes = () => {
+        let name = this.props.GeneralStore.name
+        let numPeople = this.props.GeneralStore.numPeople
+        this.props.RestaurantStore.addRes(name, numPeople)
+    }
+   
+
+    render() {
+        console.log(this.props.RestaurantStore.reservations)
         return (
             <div>
                 <span>You have {this.props.RestaurantStore.openTables} open tables</span>
-                {/* Add in # of people in restaurant */}
-                {/* Add in # of completed tables with id "completedTables*/}
-                <ResInput/>
-                <button id="addRes">Add Reservation</button>
+                <div>the number of people in the restaurant is {this.props.RestaurantStore.restPopulation}</div>
+                <div id="completedTables">the number of completed tables in the restaurant is {this.props.RestaurantStore.completedTables}</div>
+
+                <ResInput />
+
+                <button id="addRes" onClick={this.addRes}>Add Reservation</button>
+
                 {/* Make the Add Reservation button work */}
-                <div className = "reservations">
-                {/* Map reservation data to Reservation components here */}
+                <div className="reservations">
+                    {this.props.RestaurantStore.reservations.map(r => {
+
+                        console.log('hello', r)
+                        return <Reservation key={r.id} res={r} />
+                   })}
+
                 </div>
             </div>
         )
